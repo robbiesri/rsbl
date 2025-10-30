9876deafa779eae2d480479b3eb5905ac4e207b1
Robert Srinivasiah <robbies@gmail.com>
2025-10-30 00:37:40 -0700

    build first sample app
    Fixed some compiler warning issues, nice!

---
+++ apps/gltf-viewer/gltf-viewer.cpp
@@ -0,0 +1,10 @@
+// Copyright 2025 Robert Srinivasiah
+// Licensed under the MIT License, see the LICENSE file for more info
+
+#include <cstdio>
+
+int main(int /* argc */, char** /* argv */)
+{
+    printf("Hello graphics world!\n");
+    return 0;
+}
