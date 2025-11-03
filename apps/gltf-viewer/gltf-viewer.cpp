5f8cacad2284b08cd6f2ce1ba46525977e4c0f57
Robert Srinivasiah <robbies@gmail.com>
2025-11-02 22:44:35 -0800

    gltf-viewer: set up CLI11 for getting path to assets

--- apps/gltf-viewer/gltf-viewer.cpp
+++ apps/gltf-viewer/gltf-viewer.cpp
@@ -1,10 +1,22 @@
 // Copyright 2025 Robert Srinivasiah
 // Licensed under the MIT License, see the LICENSE file for more info
 
-#include <cstdio>
+#include "../../external/CLI11/CLI11.hpp"
 
-int main(int /* argc */, char** /* argv */)
+#include <cstdio>
+#include <string>
+
+#include <CLI11.hpp>
+
+int main(int argc, char** argv)
 {
+    CLI::App app("GLTF viewer");
+
+    std::string file_path;
+    app.add_option("-f,--file", file_path, "GLTF file path")->required()->check(CLI::ExistingFile);
+
+    CLI11_PARSE(app, argc, argv);
+
     printf("Hello graphics world!\n");
     return 0;
 }
